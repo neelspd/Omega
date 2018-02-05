dd82d92a74fc0a35513c2315091b4afdf1be7f59
Neel Shah <30517699+neelspd@users.noreply.github.com>
2018-02-05 12:24:58 +0530

    Another Stable Version
    A alpha Release, Needs a Little work.

--- Standalone.py
+++ Standalone.py
@@ -11,63 +11,75 @@ import matplotlib.pyplot as plt
 import serial
 import serial.tools.list_ports
 import time
-from serial.tools.list_ports_windows import NULL
+import sys
  
 fig = plt.figure()
 graph = fig.add_subplot(1,1,1)
-X_axis=[]
-Y_axis=[]
-Data=""
-ArduinoDataPort=""
-title="Creep Testing Software"
-InitTime=0.0
-root = tk.Tk()
-class StatusBar(tk.Frame):
- 
-    def __init__(self, master):
-        tk.Frame.__init__(self, master)
-        self.label = tk.Label(self, anchor=tk.W)
-        self.label.pack(fill=tk.X)
- 
-    def set(self, format, *args):
-        self.label.config(text= format % args)
-        self.label.update_idletasks()
- 
-    def clear(self):
-        self.label.config(text="")
-        self.label.update_idletasks()
-StatusLabel = StatusBar(root)
-StatusLabel.clear()
-StatusLabel.set("Initiated")
 
-StartFlag=True
-StopFlag = False
-    
+title="Creep Testing Software"
+HoursCount = ""  
+SampleID = "" 
+
+
+def getTestData(InitialTime): 
+    global HoursCount, SampleID
+    HoursTick=0
+    print(HoursCount)
+    StatusLabel.clear()
+    StatusLabel.set("Please Wait, Discovering Ports")  
+    print("Please Wait, Discovering Ports")
+    ports = list(serial.tools.list_ports.comports())
+    if ports:
+        for p in ports:
+            print(p)
+            if "(COM" in p.description:
+                StatusLabel.clear()
+                StatusLabel.set("Connected At:"+p.device)
+                print("Connected At:"+p.device) 
+            else:
+                StatusLabel.clear()
+                StatusLabel.set("No COM Port Found, Try Again")
+                print("No COM Port Found, Try Again")
+                break
+    else:
+        StatusLabel.clear()
+        StatusLabel.set("No Port Found, Try Again")
+        print("No Port Found, Try Again")        
+    ArduinoData = serial.Serial(p.device, 9600, timeout=.1)
+    Data = open(SampleID+'Test-Data.csv', 'a')
+    while HoursTick < int(HoursCount):
+        TimeLabel.clear()
+        TimeLabel.set(str(HoursTick) +" Hour(s) Passed")
+        currentTime = time.time() - InitialTime
+        currentTime = currentTime/3600 #Seconds to hour Conversion
+        HoursTick = int(currentTime)
+        while (ArduinoData.inWaiting() == 0):
+            pass
+        CharStream  = ArduinoData.readline()[:-2]
+        print(CharStream)
+        CreepValue= float(CharStream)
+        CreepPerHourValue = CreepValue/currentTime
+        Data.write(str(float(currentTime))+","+str(CreepPerHourValue)+"\n") 
+    Data.close()
+    time.sleep(1)
+                      
 ##############################################################################################
 # The below functions are the action listeners which means when buttons will be pressed the  #
 # actions described in the functions will be
 # Application are pressed then the following functions will be called and executed ###########
-##############################################################################################
-def initiateSystem():
-    global InitTime
-    InitTime = time.time()
-    
+##############################################################################################    
 def completeTest():
     StatusLabel.clear()
-    StatusLabel.set("Test Completed")
-    global StartFlag, StopFlag
-    StopFlag = True
-    
-def getTestData():
-    StatusLabel.clear()
-    StatusLabel.set("Fetching Data from System")
-    global StartFlag, StopFlag
-    StartFlag =True
-    print(StartFlag) 
-    
+    StatusLabel.set("Test Completed, Exiting")
+    time.sleep(1)
+    root.destroy()
+    sys.exit(1)
+
 def plotTestData():
-    global X_axis, Y_axis
-    graph_data = open('Test.csv','r').read()
+    X_axis=[]
+    Y_axis=[]
+    global SampleID
+    graph_data = open(SampleID+'Test-Data.csv','r').read()
     lines = graph_data.split('\n')
     for line in lines:
         if len(line) > 1:
@@ -80,65 +92,92 @@ def plotTestData():
     StatusLabel.set("Testing Completed, Plotting Graph")
     graph.plot(X_axis, Y_axis)
     plt.show()
-    
-    
 ##############################################################################################
 # Till Here ##################################################################################
 ##############################################################################################
 # GUI Starts Here ############################################################################
 ##############################################################################################
-root.title("Creep Testing Software")
-StartButton = tk.Button(root, text="Start", width=10, height=2, command = initiateSystem)
-StopButton = tk.Button(root, text="Stop", width=10, height=2, command = completeTest)
-AcquireButton = tk.Button(root, text="Acquire Data", width=10, height=2, command = getTestData)
-PlotButton = tk.Button(root, text="Plot Graph", width=10, height=2, command = plotTestData)
+root = tk.Tk()
+
+class LoginFrame(tk.Frame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.label_1 = tk.Label(self, text="Sample Id: ")
+        self.label_2 = tk.Label(self, text="Number of Operating Hours: ")
+
+        self.entry_1 = tk.Entry(self)
+        self.entry_2 = tk.Entry(self)
+
+        self.label_1.grid(row=0, sticky=tk.W,padx=10,pady=10)
+        self.label_2.grid(row=1, sticky=tk.W,padx=10,pady=10)
+        self.entry_1.grid(row=0, column=1,padx=10,pady=10)
+        self.entry_2.grid(row=1, column=1,padx=10,pady=10)
+
+
+        self.logbtn = tk.Button(self, text="GO", command = self.login_btn_clickked)
+        self.logbtn.grid(columnspan=2)
+
+        self.pack()
+
+    def login_btn_clickked(self):
+        #print("Clicked")
+        global SampleId, HoursCount
+        SampleId = self.entry_1.get()
+        HoursCount = self.entry_2.get() 
+        print(SampleId + HoursCount)
+        if SampleId == "" or HoursCount == "":
+            tk.messagebox.showerror("", "Not Enough Data")
+        else:
+            self.destroy()
+                 
+        
+class StatusBar(tk.Frame):
+ 
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        self.label = tk.Label(self, anchor=tk.W)
+        self.label.pack(fill=tk.X)
+ 
+    def set(self, format, *args):
+        self.label.config(text= format %args)
+        self.label.update_idletasks()
+ 
+    def clear(self):
+        self.label.config(text="")
+        self.label.update_idletasks()
+
+def initiateSystem():  
+    window = tk.Toplevel(root)
+    lf = LoginFrame(window)
+    StatusLabel.clear()
+    StatusLabel.set("Initiated")
+
+def initiateAcquireData():
+    InitTime = time.time()
+    getTestData(InitTime)
+        
+StatusLabel = StatusBar(root)
+StatusLabel.clear()
+StatusLabel.set("Welcome")
+TimeLabel = StatusBar(root)
+TimeLabel.clear()
+TimeLabel.set(str(HoursCount) +" Hours Passed")
+root.title(" Creep Testing ")
+StartButton = tk.Button(root, text="Start", width=13, height=2, command = initiateSystem)
+StopButton = tk.Button(root, text="Exit", width=13, height=2, command = completeTest)
+PlotButton = tk.Button(root, text="Plot Graph", width=13, height=2, command = plotTestData)
+GetDataButton = tk.Button(root, text="Get Data", width=13, height=2, command = initiateAcquireData)
 StartButton.grid(row=0,column=0, sticky=tk.W)
-AcquireButton.grid(row=0,column=1, sticky=tk.W)
-PlotButton.grid(row=0,column=3, sticky=tk.W)
-StopButton.grid(row=0,column=2, sticky=tk.W)
-StatusLabel.grid(row=2,columnspan=3, sticky=tk.W)
+GetDataButton.grid(row=0,column=1, sticky=tk.W)
+PlotButton.grid(row=0,column=2, sticky=tk.W)
+StopButton.grid(row=0,column=3, sticky=tk.W)
+StatusLabel.grid(row=2,columnspan=2, sticky=tk.W)
+TimeLabel.grid(row=2,column=2, sticky=tk.W)        
+root.mainloop()
 ##############################################################################################
 # Till Here ##################################################################################
 ##############################################################################################
-# Data Acquisition While Loop Starts Here ####################################################
-##############################################################################################
-StatusLabel.clear()
-StatusLabel.set("Please Wait, Discovering Ports")  
-print("Please Wait, Discovering Ports")
-ports = list(serial.tools.list_ports.comports())
-if ports:
-    for p in ports:
-        print(p)
-        if "(COM" in p.description:
-            ArduinoDataPort = p.device
-            StatusLabel.clear()
-            StatusLabel.set("Connected At:"+p.device)
-            print("Connected At:"+p.device) 
-        else:
-            StatusLabel.clear()
-            StatusLabel.set("No COM Port Found, Try Again")
-            print("No COM Port Found, Try Again")
-            break
-else:
-    StatusLabel.clear()
-    StatusLabel.set("No Port Found, Try Again")
-    print("No Port Found, Try Again") 
-ArduinoData = serial.Serial(ArduinoDataPort, 9600, timeout=.1)
-while StartFlag:
-    if(StopFlag):
-        break
-    Data = open('Test.csv', 'a')
-    currentTime = time.time() - InitTime
-    currentTime = currentTime/3600 #Seconds to hour Conversion
-    while (ArduinoData.inWaiting() == 0):
-        pass
-    CharStream  = ArduinoData.readline()[:-2]
-    print(CharStream)
-    CreepValue= float(CharStream)
-    CreepPerHourValue = CreepValue/currentTime
-    Data.write(str(float(currentTime))+","+str(CreepPerHourValue)+"\n")
-    Data.close() 
-    time.sleep(1)
 
-        
-root.mainloop()
+
+
